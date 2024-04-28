8969680df8bed30c3ca8ca869a51253b4ca00dac
nekoware <87574009+nekomancer69420@users.noreply.github.com>
2024-04-28 09:54:32 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -11,7 +11,7 @@ local runservice = cloneref(game:GetService("RunService"))
 local tweenservice = cloneref(game:GetService("TweenService"))
 local marketplaceservice = game:GetService("MarketplaceService")
 local textservice = cloneref(game:GetService("TextService"))
-local coregui = cloneref(gethui())
+local coregui = cloneref(game.CoreGui)
 local httpservice = cloneref(game:GetService("HttpService"))
 
 local player = players.LocalPlayer
@@ -22,6 +22,7 @@ library.theme = {
     fontsize = 15,
     titlesize = 18,
     font = Enum.Font.Code,
+    background = "rbxassetid://5553946656",
     tilesize = 90,
     cursor = false,
     cursorimg = "https://t0.rbxcdn.com/42f66da98c40252ee151326a82aab51f",
@@ -76,7 +77,7 @@ function library:CreateWatermark(name, position)
     watermark.Visible = true
     watermark.text = " " .. name:gsub("{game}", gamename):gsub("{fps}", "0 FPS") .. " "
 
-    watermark.main = Instance.new("ScreenGui", coregui)
+    watermark.main = cloneref(Instance.new("ScreenGui", coregui))
     watermark.main.Name = "Watermark"
     if syn then
         syn.protect_gui(watermark.main)
@@ -227,7 +228,7 @@ function library:CreateWindow(name, size, hidebutton)
         window.theme = (theme or library.theme)
     end
 
-    window.Main = Instance.new("ScreenGui", coregui)
+    window.Main = cloneref(Instance.new("ScreenGui", coregui))
     window.Main.Name = name
     window.Main.DisplayOrder = 15
     if syn then
@@ -1048,6 +1049,7 @@ function library:CreateWindow(name, size, hidebutton)
                     dropdown.Nav.Rotation = 90
                     dropdown.Nav.ZIndex = 5
                     dropdown.Nav.Size = UDim2.fromOffset(8, 8)
+                    dropdown.Nav.Image = "rbxassetid://4918373417"
                     dropdown.Nav.ImageColor3 = Color3.fromRGB(210, 210, 210)
     
                     dropdown.BlackOutline2 = Instance.new("Frame", dropdown.Main)
@@ -1560,6 +1562,7 @@ function library:CreateWindow(name, size, hidebutton)
                     colorpicker.hue.ZIndex = 101
                     colorpicker.hue.Position = UDim2.new(0,3,0,3)
                     colorpicker.hue.Size = UDim2.new(0,172,0,172)
+                    colorpicker.hue.Image = "rbxassetid://4155801252"
                     colorpicker.hue.ScaleType = Enum.ScaleType.Stretch
                     colorpicker.hue.BackgroundColor3 = Color3.new(1,0,0)
                     colorpicker.hue.BorderColor3 = window.theme.outlinecolor2
@@ -1573,6 +1576,7 @@ function library:CreateWindow(name, size, hidebutton)
                     colorpicker.hueselectorpointer.BorderSizePixel = 0
                     colorpicker.hueselectorpointer.Position = UDim2.new(0, 0, 0, 0)
                     colorpicker.hueselectorpointer.Size = UDim2.new(0, 7, 0, 7)
+                    colorpicker.hueselectorpointer.Image = "rbxassetid://6885856475"
 
                     colorpicker.selector = Instance.new("TextLabel", colorpicker.MainPicker)
                     colorpicker.selector.ZIndex = 100
@@ -2352,6 +2356,7 @@ function library:CreateWindow(name, size, hidebutton)
                 colorpicker.hue.ZIndex = 101
                 colorpicker.hue.Position = UDim2.new(0,3,0,3)
                 colorpicker.hue.Size = UDim2.new(0,172,0,172)
+                colorpicker.hue.Image = "rbxassetid://4155801252"
                 colorpicker.hue.ScaleType = Enum.ScaleType.Stretch
                 colorpicker.hue.BackgroundColor3 = Color3.new(1,0,0)
                 colorpicker.hue.BorderColor3 = window.theme.outlinecolor2
@@ -2365,6 +2370,7 @@ function library:CreateWindow(name, size, hidebutton)
                 colorpicker.hueselectorpointer.BorderSizePixel = 0
                 colorpicker.hueselectorpointer.Position = UDim2.new(0, 0, 0, 0)
                 colorpicker.hueselectorpointer.Size = UDim2.new(0, 7, 0, 7)
+                colorpicker.hueselectorpointer.Image = "rbxassetid://6885856475"
 
                 colorpicker.selector = Instance.new("TextLabel", colorpicker.MainPicker)
                 colorpicker.selector.ZIndex = 100
@@ -2483,6 +2489,7 @@ function library:CreateWindow(name, size, hidebutton)
                     dropdown.Nav.Rotation = 90
                     dropdown.Nav.ZIndex = 5
                     dropdown.Nav.Size = UDim2.fromOffset(8, 8)
+                    dropdown.Nav.Image = "rbxassetid://4918373417"
                     dropdown.Nav.ImageColor3 = Color3.fromRGB(210, 210, 210)
     
                     dropdown.BlackOutline2 = Instance.new("Frame", dropdown.Main)
@@ -3028,6 +3035,7 @@ function library:CreateWindow(name, size, hidebutton)
                 dropdown.Nav.Rotation = 90
                 dropdown.Nav.ZIndex = 5
                 dropdown.Nav.Size = UDim2.fromOffset(8, 8)
+                dropdown.Nav.Image = "rbxassetid://4918373417"
                 dropdown.Nav.ImageColor3 = Color3.fromRGB(210, 210, 210)
 
                 dropdown.BlackOutline2 = Instance.new("Frame", dropdown.Main)
