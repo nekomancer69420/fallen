2d5714b024b99b7060a678d524b622513e97b0fe
nekoware <87574009+nekomancer69420@users.noreply.github.com>
2024-05-14 00:43:25 +0800

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -9,9 +9,7 @@ local function saveImageFromURL(imageURL, filename)
         assert(imageContent ~= "404: Not Found", "Image not found at the provided URL")
 
         writefile(filename, imageContent)
-        rconsoleprint("Image downloaded and saved as", filename)
-    else
-        rconsoleprint("Image already exists as", filename)
+
     end
 end
 
@@ -28,8 +26,8 @@ for i,v in next, images do
     local file = "cattohook/" .. i .. ".png"
     saveImageFromURL(v, file)
     images[i] = getcustomasset(file)
-    print(images[i])
 end
+
 
 
 local library = { 
